a2cf9667126c1b5c026d5e6adaa160368f1cf05b
Coding-Seal <rodion2023@mail.ru>
2024-03-22 22:15:58 +0300

    Added a bit of code for testing

--- main.go
+++ main.go
@@ -2,10 +2,48 @@ package main
 
 import (
 	"context"
+	"github.com/redis/go-redis/v9"
+	"log/slog"
+	"os"
+	"task/floodControl"
+	"task/floodControl/redisDB"
+	"time"
 )
 
 func main() {
+	opts := &slog.HandlerOptions{
+		Level: slog.LevelDebug,
+	}
+	logger := slog.NewJSONHandler(os.Stderr, opts)
+	slog.SetDefault(slog.New(logger))
+	slog.SetLogLoggerLevel(slog.LevelDebug)
 
+	slog.Info("Connecting to db")
+	client := redis.NewClient(&redis.Options{
+		Addr:     "redis:6379",
+		Password: "", // no password set
+		DB:       0,  // use default DB})
+	})
+	err := client.Ping(context.Background()).Err()
+	if err != nil {
+		slog.Error("failed to connect to db", slog.Any("error", err))
+	}
+	var fc FloodControl = floodControl.New(redisDB.New(client), time.Second*5, 3)
+	var userID int64 = 1
+
+	for j := 0; j < 20; j++ {
+		res, err := fc.Check(context.Background(), userID)
+		if err != nil {
+			slog.Error("internal error occurred", slog.Any("error", err))
+		}
+		if res {
+			slog.Debug("access granted", slog.Int64("userID", userID))
+		} else {
+			slog.Debug("access denied", slog.Int64("userID", userID))
+		}
+		time.Sleep(time.Second)
+
+	}
 }
 
 // FloodControl интерфейс, который нужно реализовать.
